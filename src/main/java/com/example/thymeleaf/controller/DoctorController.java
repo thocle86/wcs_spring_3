eb08c7b5559dd6f613bdda36cc2ca54719d3c572
Thomas Clement <thocle86@gmail.com>
2022-03-14 22:33:00 +0100

    Ajout des fragments header et footer et résolution de problèmes divers

---
+++ src/main/java/com/example/thymeleaf/controller/DoctorController.java
@@ -0,0 +1,22 @@
+package com.example.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class DoctorController {
+
+    @GetMapping("/doctor/")
+    public String doctor(
+        @RequestParam(required=false, defaultValue="0") int number,
+        @RequestParam(required=false, defaultValue="John Smith") String name,
+        Model model
+    ) {
+            model.addAttribute("number", number);
+            model.addAttribute("name", name);
+            
+        return "doctor";
+    }
+}
